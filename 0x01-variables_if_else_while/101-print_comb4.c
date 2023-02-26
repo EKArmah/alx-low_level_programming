eb35a553ce0f1aa22c5e3f17cb757851efbb0b93
Effiba Armah <eka3782@rit.edu>
2023-02-26 15:00:00 -0800

    0x01: adv 100 - printing 2 dig num w/ restrictions

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @void: empty
+ *
+ * Description - print all combo of 3 digits
+ * Print only smallest combo of two digits
+ * Numbers should not repeat
+ * Only use putchar fcn
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int i, j, k, x;
+
+	for (i = 48; i < j; i++)
+	{
+		for (j = 49; j < x; j++)
+		{
+			for (x = 
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				if ((i != 56) || (j != 57))
+				{
+					for (k = 44; k > 31;)
+					{
+						putchar(k);
+						k -= 12;
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+
+}
