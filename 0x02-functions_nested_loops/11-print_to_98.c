a2c1a0b2d457ac74a6d279ce1ca56e94f7b3a037
Effiba Armah <eka3782@rit.edu>
2023-02-26 17:54:40 -0800

    .:

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print num up ot 98
+ * @a: starting num
+ * Return: void
+ */
+
+void print_to_98(int a)
+{
+	if (a < 98)
+	{
+		while (a < 98)
+		{
+			printf("%d, ", a);
+			a++;
+		}
+	}
+	else if (a > 98)
+	{
+		while (a > 98)
+		{
+			printf("%d, ", a);
+			a--;
+		}
+	}
+	printf("98\n");
+}
