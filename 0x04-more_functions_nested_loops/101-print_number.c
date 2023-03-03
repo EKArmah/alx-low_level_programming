cc1df0b6f053a85ed25511c5931cc8343cae871c
Effiba Armah <eka3782@rit.edu>
2023-03-02 17:26:03 -0800

    0x04: adv 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,39 @@
 #include "main.h"
+
+/**
+ * get_last_int - retrieve last int of n
+ * @s: int
+ *
+ * Return: void
+ */
+void get_last_int(int s)
+{
+	if (s < 0)
+	{
+		_putchar('-');
+		s = s * -1;
+	}
+
+	if (s == 0)
+	{
+		_putchar('0');
+	}
+
+	if (s / 10)
+	{
+		get_last_int(s / 10);
+	}
+
+	_putchar(s % 10 + '0');
+}
+
+/**
+ * print_number - prints any integer using putchar
+ * @n: int to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	get_last_int(n);
+}
