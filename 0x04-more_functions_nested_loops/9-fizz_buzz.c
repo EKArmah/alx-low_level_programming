ef3fb06e2c633149fad438cbe1719d17b98fd5ff
Effiba Armah <eka3782@rit.edu>
2023-03-02 16:40:30 -0800

    0x04: t9 - redefined spacing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,24 @@ int main(void)
 	{
 		if (count % 3 == 0 && count % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (count % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (count % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", count);
+		}
+
+		if (count != 100)
+		{
+			putchar(' ');
 		}
 	}
 	putchar('\n');
