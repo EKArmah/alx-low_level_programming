077c8dba579f225014c158b5be2410568baf09b5
Effiba Armah <eka3782@rit.edu>
2023-03-26 15:42:50 -0700

    0x0D: preprocessor tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point to print name of current file
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Understanding Preprocessor stage and Macros
