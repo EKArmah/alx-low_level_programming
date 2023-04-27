c18fee655bb4cdd72fd8af05c37d2a11cad1ce63
Effiba <effiba@aims.edu.gh>
2023-04-27 16:31:21 -0700

    0x12: t4- freeing a linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,21 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: linked list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
