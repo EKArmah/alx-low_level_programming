2a0f3757cab72b81a98c339b043bc7e4d8f59e0a
Effiba <effiba@aims.edu.gh>
2023-04-27 15:49:20 -0700

    0x12: t3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,40 @@
 #include "lists.h"
+
+/**
+ * add_node_end - adds a new node to the end of a linked list
+ * @head: pointer to a linked list
+ * @str: string to add to new node
+ *
+ * Return: address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *temp;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new_node;
+	}
+	return (new_node);
+}
