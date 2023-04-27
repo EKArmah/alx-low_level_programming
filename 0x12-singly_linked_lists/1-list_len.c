c012e85e41b7fc9e716dbf935cb9a26df7c9e4dc
Effiba <effiba@aims.edu.gh>
2023-04-27 06:47:13 -0700

    0x12: t1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,20 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * list_len - Counts the elements of a list_t
+ * @h: linked list
+ *
+ * Return: num of nodes in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
