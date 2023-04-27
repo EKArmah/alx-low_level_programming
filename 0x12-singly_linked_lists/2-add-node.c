184187f70b392f1ecbb62ae66325c1b6fe0b0987
Effiba <effiba@aims.edu.gh>
2023-04-27 08:05:23 -0700

    0x12: t2

--- 0x12-singly_linked_lists/2-add-node.c
+++ 0x12-singly_linked_lists/2-add-node.c
@@ -1,1 +1,31 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * add_node - Adds a new node to the beginning of the linked list
+ * @head - a pointer to a linked list
+ * @str: string to be used in new node
+ *
+ * Return: Address of the new node or NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (head != NULL)
+	{
+		new_node = malloc(sizeof(list_t));
+
+		if (new_node == NULL)
+		{
+			return (NULL);
+		}
+
+		new_node->str = strdup(str);
+		new_node->len = strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	return (*head);
+}
