81e619b98cf7d5f16f79579a00ac93a1c1f3b8fe
Effiba <effiba@aims.edu.gh>
2023-05-02 07:26:39 -0700

    0x12: t100 - placed construct attr incorrectly

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void fcn_first(void)
+void __attribute__ ((constructor)) fcn_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
