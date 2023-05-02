0447e0eeb37091dfd1bae568fc1d56968ad90366
Effiba <effiba@aims.edu.gh>
2023-05-02 07:17:01 -0700

    0x12: t100 - learning about constructor and destructor functions

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,13 @@
-#include "list.h"
+#include "lists.h"
+
+/**
+ * fcn_first - prints a string before main is called
+ *
+ * Return: void
+ */
+
+void fcn_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
