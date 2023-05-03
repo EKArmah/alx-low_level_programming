275cc757912d94df0c3c1e50b5bb2f79c381f0d8
Effiba <effiba@aims.edu.gh>
2023-05-02 17:06:25 -0700

    0x13: t6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+
+/**
+ * pop_listint - deletes the head of a node of a listint_t and returns its data
+ * @head: pointer to a pointer to a listint_t
+ *
+ * Return: data of node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		data = temp->n;
+		free(temp);
+	}
+	else
+	{
+		return (0);
+	}
+	return (data);
+}
