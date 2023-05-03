99d48b1ce7afb19ceb08777fa7baf82729b69b01
Effiba <effiba@aims.edu.gh>
2023-05-02 17:23:14 -0700

    0x13: t7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,27 @@
 #include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t
+ * @head: pointer to listint_t
+ * @index: nth node to retrieve
+ *
+ * Return: pointer to node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter;
+
+	if (head != NULL && index > 0)
+	{
+		for (counter = 0; (counter < index) && head; counter++)
+		{
+			head = head->next;
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (head);
+}
