0241bcaf5d699725fd6cfe3213616973f2bd4140
Effiba <effiba@aims.edu.gh>
2023-05-02 17:30:03 -0700

    0x13: t8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * sum_listint - sums all data of each node of a listint_t
+ * @head: pointer to a listint_t
+ *
+ * Return: 0 if empty, else sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
