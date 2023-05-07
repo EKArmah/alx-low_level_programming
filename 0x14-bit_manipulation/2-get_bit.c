344c17cc7cb01dd4830431e309c962fe27b2ef9b
Effiba <effiba@aims.edu.gh>
2023-05-07 10:02:24 -0700

    0x14:t2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,50 @@
 #include "main.h"
+
+/**
+ * count_bits - counts the number of bits in the binary rep of n
+ * @n: decimal
+ * @counter: current number of bits
+ *
+ * Return: number of bits
+ */
+
+unsigned long int count_bits(unsigned long int n, unsigned long int counter)
+{
+	if (n == 0 || n == 1)
+	{ 
+		return (1);
+	}
+
+	return (counter + count_bits(n >> 1, counter));
+}
+
+/**
+ * get_bit - Retrieve the value of a bit at a given index
+ * @n: value in decimal
+ * @index: index
+ *
+ * Return: value or -1 if error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int counter = 1;
+	int bit_value;
+
+	/*Count number of bits in binary of n*/
+
+	counter = count_bits(n, counter);
+	if (index >= counter || ((index % 1 < 1) && (index % 1 > 0)))
+	{
+		return (-1);
+	}
+
+	if (counter == 1)
+	{
+		return (n & 1);
+	}
+
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
+}
