1c82b0ac6589c370b57f926dfb46a71f1ea826c4
Effiba <effiba@aims.edu.gh>
2023-05-07 19:29:09 -0700

    0x14: t3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,32 @@
 #include "main.h"
+
+#define CHAR_BYTE 8
+#define MAX_ULONG (sizeof(unsigned long int) * CHAR_BYTE)
+
+/**
+ * set_bit - sets value of a bit to 1 at a given index
+ * @n: value to alter
+ * @index: location of change of bit
+ *
+ * Return: 1 if successful, -1 if error occurred
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int temp_bits_end = 0;
+
+	if (index <= MAX_ULONG)
+	{
+		/*Right shift n by index, and | 1 to affect index*/
+		temp_bits_end = *n;
+		temp_bits_end = (*n >> index) | 1;
+
+		/*Shift back to create empty canvas to be filled*/
+		temp_bits_end <<= index;
+		temp_bits_end |= *n;
+
+		*n = temp_bits_end;
+		return (1);
+	}
+	return (-1);
+}
